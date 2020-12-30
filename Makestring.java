c7b9f6c56cf0eec781d515286b51b2c1178adb27
MUSKAN MAHAJAN <45875834+muskanmahajan37@users.noreply.github.com>
2020-12-31 00:23:54 +0530

    Add files via upload

---
+++ Makestring.java
@@ -0,0 +1,11 @@
+class Makestring
+{
+ public static void main(String Args[])
+ {
+  char c[]={'J','A','V','A'};
+  String s1=new String(c);
+  String s2=new String(s1);
+  System.out.println(s1);
+    System.out.println(s2);
+}
+}
